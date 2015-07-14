ad62582179fa36868bfa98a6b7c6fcd4fdabd781
Scott Davis <stdavis@utah.gov>
2015-07-14 11:39:09 -0600

    change name from atom-amdbutler to amdbutler

--- spec/atom-amdbutler-spec.js
+++ spec/atom-amdbutler-spec.js
@@ -4,7 +4,7 @@ describe('AtomAmdButler', function () {
     var workspaceElement;
     beforeEach(function (done) {
         workspaceElement = atom.views.getView(atom.workspace);
-        atom.packages.activatePackage('atom-amdbutler').then(done);
+        atom.packages.activatePackage('amdbutler').then(done);
     });
     it('sanity', function () {
         expect(true).toBe(true);
